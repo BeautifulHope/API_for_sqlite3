b5e36609922959b1c43b31ea313a6ed4ca46f7f8
BeautifulHope <l13080349916@outlook.com>
2021-12-10 08:28:10 +0800

    Add python connect sqlite3

--- python_sqlite3/main.py
+++ python_sqlite3/main.py
@@ -3,4 +3,11 @@
 
 import sqlite3
 
-con = sqlite3.connect('example.db')
+if __name__=='__main__':
+    con = sqlite3.connect('tysql.sqlite')
+    cursor = con.cursor()
+    query = 'SELECT * FROM sqlite_master WHERE TYPE="table" AND NAME="{table_name}"'.format(table_name='Customers')
+    for i in  cursor.execute(query):
+        print(type(i))
+        print(list(i))
+        [print(a) for a in list(i)]
